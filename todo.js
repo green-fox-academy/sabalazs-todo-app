5927b0881afe9710f94d4384667a5e1bc9e69b48
sabalazs <sabalazs@gmail.com>
2020-12-10 13:24:46 +0100

    implemented printTodoList function

---
+++ todo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import minimist from 'minimist';
+import fs from 'fs';
+
+
+const args = minimist(process.argv.slice(2));
+
+if (args.l) {
+    printTodoList();
+} else {
+    printManual();
+}
+
+function printManual() {
+    console.log('\nParancssori Todo applikáció');
+    console.log('===============================\n');
+    console.log('Parancssori argumentumok:');
+    console.log('-l   Kilistázza a feladatokat');
+    console.log('-a   Új feladatot ad hozzá');
+    console.log('-r   Eltávolít egy feladatot');
+    console.log('-c   Teljesít egy feladatot');
+}
+
+function printTodoList() {
+    //reading from file
+    let todoList = fs.readFileSync('todo-data.txt', 'utf8').split(';');
+
+    //removing line breaks
+    todoList.forEach((element, index) => todoList[index] = todoList[index].trim());
+
+    //printing
+    todoList.forEach((item, index) => console.log(`${index + 1}. - ${item}`));
+}
