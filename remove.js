a4c175f5252a2893d4753f1cfcb25e13d56c8a20
sabalazs <sabalazs@gmail.com>
2020-12-10 21:35:02 +0100

    added console message about removal

--- remove.js
+++ remove.js
@@ -2,9 +2,11 @@
 
 import { getTodoList } from "./getTodoList.js";
 import { saveList } from "./saveList.js";
+import { printMessage } from "./printMessage.js";
 
 export function remove(index) {
     const newList = getTodoList();
-    newList.splice(index + -1,1);
-    saveList(newList);
+    printMessage(`${newList[index - 1].description} was removed.`);
+    newList.splice(index - 1,1);
+    saveList(newList);    
 }
