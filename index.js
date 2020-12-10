9d806a93210997cae2c1faef8e4953edfa958b7e
sabalazs <sabalazs@gmail.com>
2020-12-10 17:03:51 +0100

    removing old files

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.log('\nParancssori Todo applikáció');
-console.log('===============================\n');
-console.log('Parancssori argumentumok:');
-console.log('-l   Kilistázza a feladatokat');
-console.log('-a   Új feladatot ad hozzá');
-console.log('-r   Eltávolít egy feladatot');
-console.log('-c   Teljesít egy feladatot');
