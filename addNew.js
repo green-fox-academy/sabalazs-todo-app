483ead09570b9bbaa3fb79022883999e5234ecb7
sabalazs <sabalazs@gmail.com>
2020-12-10 21:40:39 +0100

    fixed the issue of empty todos appearing

--- addNew.js
+++ addNew.js
@@ -7,10 +7,10 @@ export function addNew(todo, file = filePath) {
     if (typeof(todo) === 'boolean') throw new Error("Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!");
 
     if (fs.existsSync(file)) {
-        fs.appendFileSync(file, `;\n${todo}`);
+        fs.appendFileSync(file, `${todo};\n`);
     } else {
         fs.writeFileSync(file, '');
-        fs.appendFileSync(file, `${todo}`);
+        fs.appendFileSync(file, `${todo}\n`);
     }
     printMessage(`${todo} was added.`);
 }
