3af1064dc3326aebad4373104a23c4d364f3f824
sabalazs <sabalazs@gmail.com>
2020-12-11 20:02:51 +0100

    removed unused import

--- addNew.js
+++ addNew.js
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import { filePath } from './config.js';
 import { getList } from './getList.js';
 import { printMessage } from './printMessage.js';
@@ -8,7 +7,6 @@ import { Todo } from './TodoClass.js';
 export function addNew(todo, file = filePath) {
 
     if (typeof(todo) === 'boolean') throw new Error("Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!");
-
     
     let todoList = getList();
     todoList.push(new Todo(todoList.length + 1, todo));
