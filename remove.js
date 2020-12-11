ae44aaa84745a157033bed6d74ab5a71198c95cd
sabalazs <sabalazs@gmail.com>
2020-12-11 19:18:17 +0100

    added error message if parameter is not a number

--- remove.js
+++ remove.js
@@ -6,12 +6,13 @@ import { printMessage } from "./printMessage.js";
 
 export function remove(index) {
 
-    if (typeof(index) !== 'number') throw new Error("Nem lehetséges az eltávolítás: nem adott meg indexet!");
+    if (typeof(index) === 'boolean') throw new Error("Nem lehetséges az eltávolítás: nem adott meg indexet!");
+    if (typeof(index) !== 'number') throw new Error("Nem lehetséges az eltávolítás: a megadott index nem szám!");
     
     const list = getList();
     if (index < 1 || list.length < index) throw new Error("Nem lehetséges az eltávolítás: túlindexelési probléma adódott!");
 
-    printMessage(`${list[index - 1].description} was removed.`);
+    printMessage(`"${list[index - 1].description}" was removed.`);
     list.splice(index - 1,1);
     saveList(list);    
 }
