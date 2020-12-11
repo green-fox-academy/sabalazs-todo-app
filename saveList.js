d21e1db83b5ca0c6b870fe12a73009956e134609
sabalazs <sabalazs@gmail.com>
2020-12-11 20:31:07 +0100

    removed unused import

--- saveList.js
+++ saveList.js
@@ -1,10 +1,9 @@
 'use strict';
 import fs from 'fs';
 import { filePath } from './config.js';
-import { printMessage } from './printMessage.js';
 
 export function saveList(list, file = filePath) {
 
     fs.writeFileSync(file, JSON.stringify(list, null, 4));
-    
+
 }
