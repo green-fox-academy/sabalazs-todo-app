643251d67abbdb2a4c2a659109647a62a1e5a214
sabalazs <sabalazs@gmail.com>
2020-12-11 09:21:13 +0100

    added error message for not number input as index

--- remove.js
+++ remove.js
@@ -5,6 +5,9 @@ import { saveList } from "./saveList.js";
 import { printMessage } from "./printMessage.js";
 
 export function remove(index) {
+
+    if (typeof(index) !== 'number') throw new Error("Nem lehetséges az eltávolítás: nem adott meg indexet!");
+
     const newList = getTodoList();
     printMessage(`${newList[index - 1].description} was removed.`);
     newList.splice(index - 1,1);
